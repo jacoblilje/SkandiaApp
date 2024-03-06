8965597ef20658718a210811c4aa905a0ae7b823
jacoblilje <162449979+jacoblilje@users.noreply.github.com>
2024-03-06 15:15:17 +0100

    05_💲_Sparkalkylatorn.py

--- 05_💲_Sparkalkylatorn.py
+++ 05_💲_Sparkalkylatorn.py
@@ -8,7 +8,6 @@ from turtle import color
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import time
 from PIL import Image
 import plotly.graph_objects as go
